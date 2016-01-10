d3611723a1797c9637f4fdbe3c5539919c05def5
HanedaKentarou <jumbo@HanedaKentarou-no-MacBook-Pro.local>
2016-01-10 18:31:46 +0900

    casing追加

--- codic_tool.go
+++ codic_tool.go
@@ -8,7 +8,9 @@ import (
 	"flag"
 	"log"
 	"os"
- )
+
+	"strings"
+)
 
 type Response struct {
 	Successful      bool
@@ -16,26 +18,42 @@ type Response struct {
 	Translated_text string
 }
 
-const CODIC_TOKEN_PATH  = "/tmp/token_codic"
-const DEFALUT_TOKEN_STR = "default"
+const CODIC_TOKEN_PATH   = "/tmp/token_codic"
+const CODIC_CASING_PATH  = "/tmp/casing_codic"
+const DEFALUT_TOKEN_STR  = "default"
+const DEFALUT_CASING_STR = "camel"
 
 func main() {
+	casing := setUpOptionsAndGetCasing()
+	args   :=  flag.Args()
 
-	// token, query
-	token := flag.String("token",DEFALUT_TOKEN_STR,"initial setting token.")
-	flag.Parse()
-
-	args := flag.Args()
 	if len(args) > 0{
-		if *token != DEFALUT_TOKEN_STR{
-			setAccessTokenToTmp(*token)
-		}
-		getCodic(args[0])
+		getCodic(args[0], casing)
 	}else{
 		flag.Usage()
 		os.Exit(0)
 	}
 }
+
+func setUpOptionsAndGetCasing() string{
+	// token, query
+	token  := flag.String("token", DEFALUT_TOKEN_STR, "initial setting token.")
+	casing := flag.String("casing",DEFALUT_CASING_STR,"[camel, pascal, lower_underscore, upper_underscore, hyphen]")
+	flag.Parse()
+	c := *casing
+
+	if *token != DEFALUT_TOKEN_STR{
+		setAccessTokenToTmp(*token)
+	}
+	if c != DEFALUT_CASING_STR {
+		setCasingToTmp(c)
+	}else {
+		c = getCasingFromTmp()
+	}
+	return c
+}
+
+// MARK: AccessToken
 
 func setAccessTokenToTmp(token string){
 	content := []byte(token)
@@ -52,10 +70,32 @@ func getAccessTokenFromTmp() string{
 	return string(contents)
 }
 
-func getCodic(query string){
+// MARK: Casing
+
+func setCasingToTmp(token string){
+	content := []byte(token)
+	ioutil.WriteFile(CODIC_CASING_PATH, content, os.ModePerm)
+}
+
+func getCasingFromTmp() string{
+	// ファイルの読み込み
+	contents,err := ioutil.ReadFile(CODIC_CASING_PATH) // ReadFileの戻り値は []byte
+	if err != nil {
+		return DEFALUT_CASING_STR
+	}
+	return string(contents)
+}
+
+// MARK: Codic
+
+func getCodic(query string, casing string){
 	// build url
 	url := "https://api.codic.jp/v1/engine/translate.json?text="
 	url += urlencode(query)
+	// CLI引数の関係で_をwhite spaceに変換
+	url += "&casing="+urlencode(strings.Replace(casing,"_"," ",-1))
+	//println(url)
+	//os.Exit(0)
 
 	client := &http.Client{}
 
